d49e018b2cbcb57e9623e18f41d7d52ce421bebe
unknown <ritikrai2111@gmail.com>
2024-03-13 11:55:21 +0530

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "medishikshatrial2003929eeeb7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s389341b36": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
